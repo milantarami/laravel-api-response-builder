e7bcf9210fb172a4956a1bd4abff91a1d73d9e34
Milan Tarami <milantarami.dev@gmail.com>
2022-01-17 23:26:48 +0545

    ✨ feat : support localization

---
+++ config/laravel-api-response-builder.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Laravel API Response Builder config
+ *
+ *
+ */
+return [
+    /**
+     * default locale will be app locale
+     *
+     */
+    "locale" => env("LARAVEL_API_RESPONSE_BUILDER_LOCALE"),
+];
